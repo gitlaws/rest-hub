e9689e615af02bff4afe882b73270b78a7a234c2
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2024-08-15 20:14:19 -0400

    refactor: Update app configuration and routes
    This commit refactors the app configuration and routes. In the `app.config.ts` file, the `appConfig` object has been updated to include the `provideZoneChangeDetection`, `provideRouter`, and `provideClientHydration` providers. The `app.routes.ts` file has been simplified by removing the unnecessary `NgModule` decorator and exporting the `routes` array directly.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,9 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes } from '@angular/router';
 import { UserFormComponent } from './features/components/user-form/user-form.component';
 import { UserListComponent } from './features/components/user-list/user-list.component';
 
-const routes: Routes = [
+export const routes: Routes = [
   { path: '', component: UserListComponent },
   { path: 'add-user', component: UserFormComponent },
   { path: 'edit-user/:id', component: UserFormComponent },
 ];
-
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
-})
-export class AppRoutingModule {}
