14edf798cc596f553d95bc8dad605cb66b2b00be
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2024-08-15 20:17:33 -0400

    feat: Add user management component and buttons for interaction
    This commit adds the necessary components for user management, including the user form and user list. It also adds buttons for adding, deleting, and updating users. This enhances the functionality of the user management feature.
    
    Note: The recent user commits and repository commits do not provide any additional relevant information for generating the commit message.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { UserManagementComponent } from './features/components/user-management/user-management.component';
+import { UserListComponent } from './features/components/user-list/user-list.component';
+import { UserFormComponent } from './features/components/user-form/user-form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    UserManagementComponent,
+    UserListComponent,
+    UserFormComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'rest-hub';
--- src/app/features/components/user-management/user-management.component.ts
+++ src/app/features/components/user-management/user-management.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { UserFormComponent } from '../user-form/user-form.component';
+import { UserListComponent } from '../user-list/user-list.component';
 
 @Component({
   selector: 'app-user-management',
   standalone: true,
-  imports: [],
+  imports: [UserFormComponent, UserListComponent],
   templateUrl: './user-management.component.html',
-  styleUrl: './user-management.component.scss'
+  styleUrl: './user-management.component.scss',
 })
 export class UserManagementComponent {
-
+  addUser() {
+    throw new Error('Method not implemented.');
+  }
+  deleteUser() {
+    throw new Error('Method not implemented.');
+  }
+  updateUser() {
+    throw new Error('Method not implemented.');
+  }
 }
