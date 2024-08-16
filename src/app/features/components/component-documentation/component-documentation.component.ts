bbbd5de580b3bebe2f1b89bba51d5648694e3aba
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2024-08-15 23:42:39 -0400

    chore: Update component-documentation to use UserFormComponent instead of UserManagementComponent
    This commit updates the component-documentation.component.html and component-documentation.component.ts files to replace the commented out <app-user-management> component with the <app-user-form> component. This change aligns the component with the updated functionality of the application.
    
    Note: The recent user commits and repository commits do not provide any additional relevant information for generating the commit message.

--- src/app/features/components/component-documentation/component-documentation.component.ts
+++ src/app/features/components/component-documentation/component-documentation.component.ts
@@ -2,11 +2,17 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { CardComponent } from '../card/card.component';
 import { UserManagementComponent } from '../user-management/user-management.component';
+import { UserFormComponent } from '../user-form/user-form.component';
 
 @Component({
   selector: 'app-component-documentation',
   standalone: true,
-  imports: [CommonModule, CardComponent, UserManagementComponent],
+  imports: [
+    CommonModule,
+    CardComponent,
+    UserManagementComponent,
+    UserFormComponent,
+  ],
   templateUrl: './component-documentation.component.html',
   styleUrl: './component-documentation.component.scss',
 })
