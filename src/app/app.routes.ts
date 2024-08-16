7a56e5d77858656f7e683be08b8af4f4a61750ab
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2024-08-15 23:40:44 -0400

    chore: Update app.component.html, app.component.ts, toolbar.component.html, and db.json
    This commit updates the app.component.html file to comment out the <app-user-management> component and add a <router-outlet> and <app-component-documentation> component. It also updates the app.component.ts file to import the ComponentDocumentationComponent. Additionally, it comments out the <app-cyber-menu> component in the toolbar.component.html file. Finally, it adds a new user object to the db.json file.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,17 @@
 import { Routes } from '@angular/router';
 import { UserFormComponent } from './features/components/user-form/user-form.component';
 import { UserListComponent } from './features/components/user-list/user-list.component';
+import { ComponentDocumentationComponent } from './features/components/component-documentation/component-documentation.component';
 
 export const routes: Routes = [
-  { path: '', component: UserListComponent },
-  { path: 'add-user', component: UserFormComponent },
-  { path: 'edit-user/:id', component: UserFormComponent },
+  {
+    path: 'components',
+    loadComponent: () =>
+      import(
+        './features/components/component-documentation/component-documentation.component'
+      ).then((component) => component.ComponentDocumentationComponent),
+  },
+  // { path: '', component: UserListComponent },
+  // { path: 'add-user', component: UserFormComponent },
+  // { path: 'edit-user/:id', component: UserFormComponent },
 ];
--- src/app/features/components/component-documentation/component-documentation.component.ts
+++ src/app/features/components/component-documentation/component-documentation.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { CardComponent } from '../card/card.component';
+import { UserManagementComponent } from '../user-management/user-management.component';
 
 @Component({
   selector: 'app-component-documentation',
   standalone: true,
-  imports: [CommonModule, CardComponent],
+  imports: [CommonModule, CardComponent, UserManagementComponent],
   templateUrl: './component-documentation.component.html',
   styleUrl: './component-documentation.component.scss',
 })
