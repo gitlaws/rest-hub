f38db5743ea9027550ef315e2915c7aaec586e73
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2024-08-15 20:43:14 -0400

    chore: Update background color in app.component.scss and user-management.component.scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,15 +3,19 @@ import { RouterOutlet } from '@angular/router';
 import { UserManagementComponent } from './features/components/user-management/user-management.component';
 import { UserListComponent } from './features/components/user-list/user-list.component';
 import { UserFormComponent } from './features/components/user-form/user-form.component';
+import { ToolbarComponent } from './features/components/toolbar/toolbar.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
+    CommonModule,
     RouterOutlet,
     UserManagementComponent,
     UserListComponent,
     UserFormComponent,
+    ToolbarComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
