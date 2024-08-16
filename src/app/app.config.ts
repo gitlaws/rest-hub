40e3fda9b2ce502edd7f64bb0eec93510d45d427
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2024-08-15 22:48:35 -0400

    chore: Update app.config.ts to provide HttpClient with Fetch
    This commit updates the app.config.ts file to import the provideHttpClient and withFetch functions from the @angular/common/http module. It then adds the provideHttpClient(withFetch()) function to the list of providers in the appConfig constant. This change enables the use of the Fetch API for HTTP requests in the application.
    
    Note: The recent user commits and repository commits do not provide any additional relevant information for generating the commit message.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,6 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 import { routes } from './app.routes';
 ``;
@@ -10,5 +11,6 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideClientHydration(),
+    provideHttpClient(withFetch()),
   ],
 };
