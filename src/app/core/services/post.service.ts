c6351b4a640a81fd14bf1e139ac1e0a4f3b52aa5
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2024-08-28 20:16:21 -0400

    chore: Update PostService to use HttpClient for API requests
    This commit updates the PostService to use the HttpClient module from @angular/common/http for making API requests. The getPosts() method now sends a GET request to the specified API endpoint to fetch the list of posts. Additionally, a new createPost() method has been added to send a POST request for creating a new post. This change improves the functionality of the PostService by utilizing the HttpClient module for efficient and standardized API communication.

--- src/app/core/services/post.service.ts
+++ src/app/core/services/post.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { Post } from '../models/post';
 
@@ -6,11 +7,15 @@ import { Post } from '../models/post';
   providedIn: 'root',
 })
 export class PostService {
-  private posts: Post[] = [];
+  private apiUrl = 'https://your-backend-url/api/posts';
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
   getPosts(): Observable<Post[]> {
-    return of(this.posts);
+    return this.http.get<Post[]>(this.apiUrl);
+  }
+
+  createPost(post: Post): Observable<Post> {
+    return this.http.post<Post>(this.apiUrl, post);
   }
 }
