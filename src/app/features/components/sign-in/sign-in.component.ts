e6d23c297418066472a639c19ad40654865ed0b7
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2024-09-05 21:34:18 -0400

    Refactor sign-in component to use separate methods for API requests

--- src/app/features/components/sign-in/sign-in.component.ts
+++ src/app/features/components/sign-in/sign-in.component.ts
@@ -2,20 +2,22 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../../../core/services/auth.service';
+import { FormsModule } from '@angular/forms';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-in',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './sign-in.component.html',
-  styleUrl: './sign-in.component.scss',
+  styleUrls: ['./sign-in.component.scss'],
 })
 export class SignInComponent {
   errorMessage: string | null = null;
 
   constructor(private authService: AuthService, private router: Router) {}
 
-  onSubmit(form: any) {
+  onSubmit(form: NgForm) {
     const { email, password } = form.value;
 
     this.authService.signIn(email, password).subscribe(
